aa79668ddb08daeef849209af8d8bd14cb7df6cd
Guilherme Illescas <gui.illescas@gmail.com>
2021-04-04 19:22:46 -0300

    insert root reducer on store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,12 @@
 import { createStore } from 'redux';
+import { ICartState } from './modules/cart/types';
 
-const store = createStore(() => []);
+import rootReducer from './modules/rootReducer';
+
+export interface IState {
+  cart: ICartState
+}
+
+const store = createStore(rootReducer);
 
 export default store;
